7ce3a2ee36196e99350cfb79f807ccd95819654a
Patrick McGee <pat.mcmcgee@gmail.com>
2013-04-08 01:29:55 -0400

    styling, back to spaces not tabs

--- insertion_asc.py
+++ insertion_asc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 def insertion_sort_asc(list):
-	for j in range(len(list)):
-		key = list[j]
-		i = j - 1
-		while (i >= 0 and list[i] > key):
-			list[i + 1] = list[i]
-			i = i - 1
-		list[i + 1] = key
+  for j in range(len(list)):
+    key = list[j]
+    i = j - 1
+    while (i >= 0 and list[i] > key):
+      list[i + 1] = list[i]
+      i = i - 1
+    list[i + 1] = key
--- insertion_desc.py
+++ insertion_desc.py
@@ -1,10 +1,8 @@
 def insertion_sort_desc(list):
-	for j in range(1,len(list)):
-		key = list[j]
-		i = j - 1
-		print len(list)
-		print i
-		while (i >= 0 and list[i] < key):
-			list[i+1] = list[i]
-			i = i - 1
-		list[i+1] = key
+  for j in range(1,len(list)):
+    key = list[j]
+    i = j - 1
+    while (i >= 0 and list[i] < key):
+      list[i+1] = list[i]
+      i = i - 1
+    list[i+1] = key
