27a8706cc3f33da11ca8e9c7bde045885e721564
Patrick McGee <pat.mcmcgee@gmail.com>
2013-04-08 01:09:33 -0400

    finished up insertion asc and desc, needed to figure out what I was doing wrong with desc

--- insertion_asc.py
+++ insertion_asc.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 def insertion_sort_asc(list):
-	for j in range(2,len(list)):
+	for j in range(len(list)):
 		key = list[j]
 		i = j - 1
-		while i > 0 and list[i] > key:
+		while (i >= 0 and list[i] > key):
 			list[i + 1] = list[i]
 			i = i - 1
-
 		list[i + 1] = key
 
 
--- sort_runner.py
+++ sort_runner.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
 import insertion_asc
+import insertion_desc
+a = [100,30,40,50,22,8,13,4,7,9,10,15]
+b = [0,30,40,50,22,8,13,4,7,9,10,15]
 
-a = [30,40,50,22,8,13,4,7,9,10,15]
-b = a
-c = a
-
+print 'asc'
+print 'before' 
 print a # print before sort
 insertion_asc.insertion_sort_asc(a)
+print 'after' 
 print a # print after sort
+
+print 'desc'
+print 'before' 
+print b # print before sort
+insertion_desc.insertion_sort_desc(b)
+print 'after' 
+print b # print after sort
